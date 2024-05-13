ea61ec2fb08270328e751ead2ccf80719b1b5f53
Anna <anna.w099@gmail.com>
2024-05-13 15:03:56 +0200

    addform2

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -27,3 +27,40 @@ const AddTodo: React.FC =() => {
 
 
   export default  AddTodo
+
+
+
+
+// import React, { useState } from 'react';
+// import { useAppDispatch } from '../../hooks';
+// import { addTodo } from '../../store/slice/todoSice';
+
+
+// const AddTodo: React.FC = () => {
+//     const dispatch = useAppDispatch();
+//     const [title, setTitle] = useState('');
+
+//     const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+//         setTitle(e.target.value);
+//     };
+
+//     const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+//         e.preventDefault();
+//         dispatch(addTodo(title));
+//         setTitle(''); // Очищаем значение после добавления задачи
+//     };
+
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <input 
+//                 type="text" 
+//                 name='title' 
+//                 value={title} // Привязываем значение инпута к состоянию title
+//                 onChange={handleChange} // Обработчик изменения для обновления состояния title
+//             />
+//             <button type="submit">Hinzufügen</button>
+//         </form>
+//     );
+// };
+
+// export default AddTodo;
